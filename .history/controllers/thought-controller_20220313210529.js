35130194cf599add18fe1c0f243304f4e18c3101
brenhamp <bpozzi@trinity.edu>
2022-03-13 21:06:37 -0500

    added more specific error messages

--- .history/controllers/thought-controller_20220313210529.js
+++ .history/controllers/thought-controller_20220313210529.js
@@ -21,12 +21,12 @@ const thoughtController = {
         })
         .select('-__v')
         .sort({_id: -1})
-        .then((dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No hought found with this id!' });
+        .then((dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
                 return;
             }
-            res.json(dbThoughtData)
+            res.json(dbUserData)
         }))
         .catch(err => res.status(400).json(err));
     },
--- .history/controllers/user-controller_20220313210508.js
+++ .history/controllers/user-controller_20220313210508.js
@@ -30,14 +30,11 @@ const userController = {
         })
         .select('-__v')
         .sort({_id: -1})
-        .then((dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id!' });
-                return;
-            }
-            res.json(dbUserData)
-        }))
-        .catch(err => res.status(400).json(err));
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
     },
 
     //POST new user
